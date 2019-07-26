7e9439d0eb772b1764d4770b6e434db0d4330076
israelcode <31614943+israelcode@users.noreply.github.com>
2019-07-26 19:37:28 +0300

    Update autoruparser.py

--- autoruparser.py
+++ autoruparser.py
@@ -64,7 +64,7 @@ def main():
     browser = webdriver.Firefox()
     f = open('BASE.csv', 'w')
 
-    for i in range(1, 10): # Number of pages you want to parse. In this case first 10 pages
+    for i in range(1, 10): # Number of pages you want to parse. In this case first 9 pages
         url_gen = base_url + str(i)
         print(url_gen) 
         browser.get(url_gen)
